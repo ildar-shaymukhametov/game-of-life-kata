55725b1becd7d5999e13803be0d7da7175444c88
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-09-23 21:06:07 +0500

    remove class 'Cells'

--- csharp/src/Program.cs
+++ csharp/src/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var world = new World();
+var random = new Random();
+var max = 30;
+for (int i = 0; i < max * 8; i++)
+{
+    world.SetLivingAt(new Coordinates(random.Next(max), random.Next(max)));
+}
+world.SetLivingAt(new Coordinates(2, 2));
+world.SetLivingAt(new Coordinates(1, 2));
+
+while (true)
+{
+    System.Console.WriteLine(world);
+    world.Tick();
+    Thread.Sleep(100);
+    System.Console.Clear();
+}
--- csharp/src/World.cs
+++ csharp/src/World.cs
@@ -1,39 +1,88 @@
+using System.Text;
+
 public class World
 {
-    private readonly Cells _cells;
+    private Cell[,] _cells;
 
     public World()
     {
-        _cells = new Cells();
+        _cells = new Cell[30, 30];
+        Iterate(x => x.Initialize(_cells));
     }
 
     public bool IsEmpty()
     {
-        return !_cells.AnyAlive();
+        return !_cells.Cast<Cell>().Any(x => x is LivingCell);
     }
 
     public void SetLivingAt(Coordinates coordinates)
     {
-        _cells.SetLivingAt(coordinates);
+        coordinates.SetLiving(_cells);
     }
 
     public void Tick()
     {
-        _cells.Tick();
-    }
+        var next = new Cell[_cells.GetLength(0), _cells.GetLength(1)];
 
-    public bool IsLivingAt(Coordinates coordinates)
-    {
-        return _cells.IsLivingAt(coordinates);
+        for (int x = 0; x < _cells.GetLength(0); x++)
+        {
+            for (int y = 0; y < _cells.GetLength(1); y++)
+            {
+                var coords = new Coordinates(x, y);
+                if (coords.IsAliveInNextGeneration(_cells))
+                {
+                    coords.SetLiving(next);
+                }
+                else
+                {
+                    coords.SetDead(next);
+                }
+            }
+        }
+
+        _cells = next;
     }
 
     public bool IsDeadAt(Coordinates coordinates)
     {
-        return _cells.IsDeadAt(coordinates);
+        return coordinates.IsDead(_cells);
+    }
+
+    private void Iterate(Action<Coordinates> action)
+    {
+        for (int x = 0; x < _cells.GetLength(0); x++)
+        {
+            for (int y = 0; y < _cells.GetLength(1); y++)
+            {
+                action(new Coordinates(x, y));
+            }
+        }
+    }
+
+    public bool IsLivingAt(Coordinates coordinates)
+    {
+        return coordinates.IsLiving(_cells);
     }
 
     public override string ToString()
     {
-        return _cells.ToString();
+        var result = new StringBuilder();
+        for (int y = 0; y < _cells.GetLength(1); y++)
+        {
+            for (int x = 0; x < _cells.GetLength(0); x++)
+            {
+                if (IsLivingAt(new Coordinates(x, y)))
+                {
+                    result.Append(" * ");
+                }
+                else
+                {
+                    result.Append("   ");
+                }
+            }
+            result.AppendLine();
+        }
+
+        return result.ToString();
     }
 }
